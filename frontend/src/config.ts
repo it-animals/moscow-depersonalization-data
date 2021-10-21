860986e376512e27707d7f068ba72099e7a07bae
Kalinss <kalinss16@gmail.com>
2021-10-21 13:16:59 +0400

    up

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,6 +3,6 @@ const isDevelopment = process.env["NODE_ENV"];
 export const isDev = () => isDevelopment === "development";
 
 export const appConfig = {
-  apiUrl: isDev() ? "http://localhost:8000/v1/" : "/v1/",
+  apiUrl: isDev() ? "http://localhost:8000/v1/" : "http://62.84.123.147:8000/v1/",
   titleApp: " | Сервис отзывов",
 };

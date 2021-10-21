e892a6a648dad64bcbbb35ed8f771f2257a07e76
Kalinss <kalinss16@gmail.com>
2021-10-21 18:29:28 +0400

    Ссылка в хедере

--- frontend/src/ui/components/header/Header.tsx
+++ frontend/src/ui/components/header/Header.tsx
@@ -6,15 +6,57 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LogoLink = styled.a`
+  text-decoration: none;
+  color: white;
+  &:active {
+    color: white;
+  }
+  &:hover {
+    color: white;
+    text-decoration: underline;
+  }
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  &:active {
+    color: white;
+  }
+  &:hover {
+    color: white;
+    text-decoration: underline;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  margin-left: 125px;
+  height: 100%;
+  align-items: center;
+  & ul {
+    display: flex;
+    column-gap: 20px;
+    align-items: center;
+    height: 100%;
+  }
+`;
+
 export const Header: CT<unknown> = () => {
   return (
     <header>
       <Box>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              Заголовок
-            </Typography>
+            <LogoLink href="/">
+              <Typography variant="h6" noWrap component="div">
+                Персонализатор
+              </Typography>
+            </LogoLink>
           </Toolbar>
         </AppBar>
       </Box>

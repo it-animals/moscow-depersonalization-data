49be37b8f4684ba55ca33eedf95876d554ffa781
Kalinss <kalinss16@gmail.com>
2021-10-23 18:09:26 +0400

    скачивание пакета

--- frontend/src/ui/components/packageStatus/PackageStatus.tsx
+++ frontend/src/ui/components/packageStatus/PackageStatus.tsx
@@ -5,7 +5,21 @@ import {
   packageInWork,
   PackageType,
 } from "../../../domain/package";
-import { Button, Typography } from "@mui/material";
+import {
+  Button,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Typography,
+} from "@mui/material";
+import { KeyboardArrowDownOutlined } from "@mui/icons-material";
+import { fileAbort, fileError } from "../../../domain/file";
+import { useRef, useState } from "react";
+import { appConfig } from "../../../config";
+import { Link } from "react-router-dom";
 
 const Status = styled.div`
   width: 100%;
@@ -17,9 +31,91 @@ const Status = styled.div`
   justify-content: space-between;
 `;
 
+const MenuLink = styled.a`
+  color: black;
+
+  &:active {
+    color: black;
+  }
+`;
+
 export const PackageStatus: CT<{ packageFile: PackageType | null }> = ({
   packageFile,
 }) => {
+  const Download: CT<{ item: PackageType }> = ({ item }) => {
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef<HTMLButtonElement | null>(null);
+    const openHandler = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    return (
+      <div>
+        <Button
+          ref={anchorRef}
+          endIcon={<KeyboardArrowDownOutlined />}
+          color={"success"}
+          variant={"contained"}
+          onClick={openHandler}
+        >
+          Скачать пакет документов
+        </Button>
+        <Popper
+          open={open}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          style={{ zIndex: 111 }}
+          anchorEl={anchorRef.current}
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                  >
+                    {!packageInWork(item) &&
+                      !packageAbort(item) &&
+                      !packageError(item) && (
+                        <MenuItem onClick={handleClose}>
+                          <MenuLink
+                            href={`${appConfig.apiUrl}task/download?id=${item.id}&pdf=0`}
+                          >
+                            в .jpeg формате
+                          </MenuLink>
+                        </MenuItem>
+                      )}
+
+                    <MenuItem onClick={handleClose}>
+                      <MenuLink
+                        href={`${appConfig.apiUrl}task/download?id=${item.id}&pdf=1`}
+                      >
+                        в .pdf формате
+                      </MenuLink>
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    );
+  };
+
   if (!packageFile) {
     return <></>;
   }
@@ -33,6 +129,7 @@ export const PackageStatus: CT<{ packageFile: PackageType | null }> = ({
         >
           Пакет документов №{packageFile.id}. Обработка отменена
         </Typography>
+        <Download item={packageFile} />
       </Status>
     );
   }
@@ -46,9 +143,7 @@ export const PackageStatus: CT<{ packageFile: PackageType | null }> = ({
         >
           Пакет документов №{packageFile.id} преобразован с ошибками
         </Typography>
-        <Button color={"warning"} variant={"contained"}>
-          Скачать полный пакет документов
-        </Button>
+        <Download item={packageFile} />
       </Status>
     );
   }
@@ -62,6 +157,7 @@ export const PackageStatus: CT<{ packageFile: PackageType | null }> = ({
         >
           Пакет документов №{packageFile.id} находится в обработке
         </Typography>
+        <Download item={packageFile} />
       </Status>
     );
   }
@@ -74,9 +170,7 @@ export const PackageStatus: CT<{ packageFile: PackageType | null }> = ({
       >
         Пакет документов №{packageFile.id} успешно преобразован
       </Typography>
-      <Button color={"success"} variant={"contained"}>
-        Скачать полный пакет документов
-      </Button>
+      <Download item={packageFile} />
     </Status>
   );
 };
